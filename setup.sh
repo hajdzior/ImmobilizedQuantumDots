928dadaa9624e0a1d0bfa0480d5863e42c99402c
Marta Waligórska <marta_h@amu.edu.pl>
2016-01-28 23:46:07 +0100

    Create all necessary temporary directories in the new folder

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ fi
 TARGET=$1
 CURRENT=`pwd`
 
-LINKS="SOFT_calcTrajs-onOffTimes RUN_calcTrajs_on-off-times.sh RUN_plot_traj.sh"
-DIRS="trajsTXT trajsPLOT thresh"
+LINKS=".git SOFT_calcTrajs-onOffTimes RUN_calcTrajs_on-off-times.sh RUN_plot_traj.sh"
+DIRS="trajsTXT trajsPLOT thresh tmp ptu on_off_analysis on_off_analysis/on_off_plots_Clausset on_off_analysis/on_off_plots_Plenz on_off_times GaussFit 2D-int_hist"
 
 mkdir $TARGET
 
